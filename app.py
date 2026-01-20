be98ff03892bf480264e4f857f1a3c6c82462370
Alice <siangping5327@gmail.com>
2026-01-20 16:17:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, jsonify
-import os
 
 app = Flask(__name__)
 
@@ -9,12 +8,6 @@ def webhook():
     print("收到 Dialogflow request:", req)
     return jsonify({"fulfillmentText": "Webhook 成功連線！🚀"})
 
-# ======================================
-# Railway 正確啟動方式
-# ======================================
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
 
 
 
